c0d61e3fb940ed7ba062c04582b4558205b62a80
ZBurNIngram <dumbledorebryant@gmail.com>
2022-01-01 20:33:51 -0500

    2022-01-01 lc225

---
+++ leetcode/src/pass/lc225.java
@@ -0,0 +1,47 @@
+package pass;
+
+import java.util.PriorityQueue;
+import java.util.Stack;
+
+import java.util.*;
+
+public class lc225 {
+    Queue<Integer> prev = new PriorityQueue<>();
+    Queue<Integer> next = new PriorityQueue<>();
+    public lc225() {
+        
+    }
+    
+    public void push(int x) {
+        prev.add(x);
+    }
+    
+    public int pop() {
+        int num = 0;
+        while (!prev.isEmpty()){
+            num = prev.poll();
+            if (prev.isEmpty()) break;
+            else next.add(num);
+        }
+        while (!next.isEmpty()){
+            prev.add(next.poll());
+        }
+        return num;
+    }
+    
+    public int top() {
+        int num = 0;
+        while (!prev.isEmpty()){
+            num = prev.poll();
+            next.add(num);
+        }
+        while (!next.isEmpty()){
+            prev.add(next.poll());
+        }
+        return num;
+    }
+    
+    public boolean empty() {
+        return prev.isEmpty();
+    }
+}
