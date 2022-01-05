589047d337b09e1d0ee92dd05904a109a8c4312e
ZBurNIngram <dumbledorebryant@gmail.com>
2022-01-05 16:52:30 -0500

    2022/01/05 a lc398

--- leetcode/src/pass/lc398.java
+++ leetcode/src/pass/lc398.java
@@ -1,58 +1,23 @@
 package pass;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class lc398 {
-    int[] numss;
+    int[] nums;
     public lc398(int[] nums) {
-        Arrays.sort(nums);
-        numss = Arrays.copyOf(nums, nums.length);
+        this.nums = nums;
     }
     
     public int pick(int target) {
-        int left = leftb(numss, target);
-        int right = rightb(numss, target);
-        return (int)(left + Math.random() * (right - left + 1));
-    }
-    public int leftb(int[] nums, int target){
-        int lindex = -1;
-        int start = 0;
-        int end = nums.length;
-        while (start < end){
-            int mid = end + ((start - end) >> 2);
-            int num = nums[mid];
-            if (target > num) {
-                start = mid + 1;
-            }
-            else if (target < num) end = mid - 1;
-            else {
-                while (nums[mid] == target){
-                    mid--;
-                }
-                lindex = mid + 1;
+        Random r = new Random();
+        int i = 0;
+        int index = 0;
+        for (int j = 0; j < nums.length; j++){
+            if (nums[j] == target){
+                i++;
+                if (r.nextInt(i) == 0) index = i;
             }
         }
-        return lindex;
-    }
-    public int rightb(int[] nums, int target){
-        int lindex = -1;
-        int start = 0;
-        int end = nums.length;
-        while (start < end){
-            int mid = end + ((start - end) >> 2);
-            int num = nums[mid];
-            if (target > num) {
-                start = mid + 1;
-            }
-            else if (target < num) end = mid - 1;
-            else {
-                while (nums[mid] == target){
-                    mid++;
-                }
-                lindex = mid - 1;
-            }
-        }
-        return lindex;
+        return index;
     }
 }
