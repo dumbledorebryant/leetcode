27e14bdfce9ea0b315dd9ad38d0ba71c659722c1
ZBurNIngram <dumbledorebryant@gmail.com>
2022-01-06 11:12:03 -0500

    2022/01/05 m lc241

---
+++ leetcode/src/pass/lc241.java
@@ -0,0 +1,53 @@
+package pass;
+import java.util.*;
+public class lc241 {
+    public List<Integer> diffWaysToCompute(String expression) {
+        char[] chrs = expression.toCharArray();
+        int length = chrs.length;
+        return collect(chrs, 0, length - 1);
+    }
+    public List<Integer> collect(char[] chrs, int left, int right){
+        List<Integer> ans = new ArrayList<>();
+        if (left == right - 1) {
+            int a = Character.getNumericValue(chrs[left]);
+            int b = Character.getNumericValue(chrs[right]);
+            ans.add(a * 10 + b);
+            return ans;
+        }
+        if (left == right) {
+            ans.add(Character.getNumericValue(chrs[left]));
+            return ans;
+        }
+        for (int i = left + 1; i < right; i += 2){
+            char oper = chrs[i];
+            if (oper == '*'){
+                List<Integer> leftnum = collect(chrs, left, i - 1);
+                List<Integer> rightnum = collect(chrs, i + 1, right);
+                for (int lnum : leftnum){
+                    for (int rnum : rightnum) {
+                        ans.add(lnum * rnum);
+                    }
+                }
+            }
+            else if (oper == '+') {
+                List<Integer> leftnum = collect(chrs, left, i - 1);
+                List<Integer> rightnum = collect(chrs, i + 1, right);
+                for (int lnum : leftnum){
+                    for (int rnum : rightnum) {
+                        ans.add(lnum + rnum);
+                    }
+                }
+            }
+            else {
+                List<Integer> leftnum = collect(chrs, left, i - 1);
+                List<Integer> rightnum = collect(chrs, i + 1, right);
+                for (int lnum : leftnum){
+                    for (int rnum : rightnum) {
+                        ans.add(lnum - rnum);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
