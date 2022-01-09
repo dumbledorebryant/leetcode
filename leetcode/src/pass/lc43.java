ed41f451e4980e4246620cd4ad626c42577a34c5
ZBurNIngram <dumbledorebryant@gmail.com>
2022-01-09 11:54:48 -0500

    2022/01/09 m lc583

--- leetcode/src/pass/lc43.java
+++ leetcode/src/pass/lc43.java
@@ -2,8 +2,24 @@ package pass;
 
 public class lc43 {
     public String multiply(String num1, String num2) {
-        public int findTargetSumWays(int[] nums, int target) {
-        
+        int m = num1.length();
+        int n = num2.length();
+        int[] dp = new int[m + n];
+
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                int a = Integer.valueOf(num1.substring(i, i + 1));
+                int b = Integer.valueOf(num2.substring(j, j + 1));
+                int num = a * b;
+                int sum = num + dp[i + j];
+                dp[i + j] += (sum % 10);
+                dp[i + j + 1] += (sum / 10);
+            }
         }
+    
+        String str = "";
+        for (int i = m + n - 1; i >= 0; i--) str += String.valueOf(dp[i]);
+        int valid = Integer.parseInt(str);
+        return String.valueOf(valid);
     }
 }
