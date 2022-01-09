fd0d8b278e3fcb15ef6e2e79a9dbf8fe0ad18044
ZBurNIngram <dumbledorebryant@gmail.com>
2022-01-09 10:41:04 -0500

    2022/01/09 m lc1143

--- leetcode/src/pass/lc1143undone.java
+++ leetcode/src/pass/lc1143undone.java
@@ -2,19 +2,21 @@ package pass;
 
 public class lc1143undone {
     public int longestCommonSubsequence(String text1, String text2) {
-        int count = 0;
-        char[] chs1 = text1.toCharArray();
-        char[] chs2 = text2.toCharArray();
-        int m = chs1.length;
-        int n = chs2.length;
-        if (m == 0 || n == 0) return 0;
-        int length = Math.min(m, n);
-        int[] array = new int[length];
-        int left = 0;
-        int right = 0;
-        for (int i = 1; i <= length; i++){
-            
+        int m = text1.length();
+        int n = text2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) dp[i][0] = 0;
+        for (int j = 0; j <= n; j++) dp[0][j] = 0;
+        for (int i = 1; i <= m; i++){
+            for (int j = 1; j <= n; j++){
+                char a = text1.charAt(i - 1);
+                char b = text2.charAt(j - 1);
+                if (a == b) dp[i][j] = dp[i - 1][j - 1] + 1;
+                else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
         }
-        return count;
+        return dp[m][n];
     }
 }
