86de70374881a37afcb95b0bece05d65e4cd33bc
ZBurNIngram <dumbledorebryant@gmail.com>
2022-01-16 15:38:22 -0500

    2022/01/16 a lc1312

--- leetcode/src/pass/lc1312.java
+++ leetcode/src/pass/lc1312.java
@@ -1,7 +1,26 @@
 package pass;
 
+import java.util.Arrays;
+
 public class lc1312 {
     public int minInsertions(String s) {
-        
+        int len = s.length();
+        int[][] dp = new int[len][len];
+        for (int i = 0; i < len; i++){
+            dp[i][i] = 0;
+        }
+        for (int i = len - 2; i >= 0; i--){
+            for (int j = i + 1; j < len; j++){
+                char a = s.charAt(i);
+                char b = s.charAt(j);
+                if (a == b) {
+                    dp[i][j] = dp[i + 1][j - 1];
+                }
+                else {
+                    dp[i][j] = Math.min(dp[i + 1][j], dp[i][j - 1]) + 1;
+                }
+            }
+        }
+        return dp[0][len - 1];
     }
 }
