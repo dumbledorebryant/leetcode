613e2ff807ca41420b5cd8077e972bd51d3b8f5f
ZBurNIngram <dumbledorebryant@gmail.com>
2022-01-26 16:23:15 -0500

    2022/01/26 a lc53

--- leetcode/src/pass/lc53.java
+++ leetcode/src/pass/lc53.java
@@ -3,24 +3,16 @@ package pass;
 public class lc53 {
     public int maxSubArray(int[] nums) {
         int length = nums.length;
-        if (length == 1) return nums[0];
-        int[] array = new int[length + 1];
-        array[0] = 0;
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < length; i++){
-            if (array[i] > 0) {
-                array[i + 1] = array[i] + nums[i];
-            }
-            else array[i + 1] = nums[i];
+        int[] dp = new int[length + 1];
+        dp[0] = 0;
+        int ans = Integer.MIN_VALUE;
+        for (int i = 1; i <= length; i++){
+            int num = nums[i - 1];
+            if (dp[i - 1] > 0)
+                dp[i] = dp[i - 1] + num;
+            else dp[i] = num;
+            ans = Math.max(ans, dp[i]);
         }
-        for (int i = 1; i < length; i++){
-            max = Math.max(max, array[i]);
-        }
-        return max;
-    }
-
-    public void test(){
-        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
-        System.out.println(maxSubArray(nums));
+        return ans;
     }
 }
