2e1d6ad2478eb98a2e0cc92550c085ea5bedf115
ZBurNIngram <dumbledorebryant@gmail.com>
2022-01-26 16:57:08 -0500

    2022/01/26 a lc38

--- leetcode/src/pass/lc38.java
+++ leetcode/src/pass/lc38.java
@@ -2,6 +2,22 @@ package pass;
 
 public class lc38 {
     public String countAndSay(int n) {
-        
+        String ans = "1";
+        for (int i = 2; i <= n; i++){
+            int m = ans.length();
+            String cur = "";
+            for (int j = 0; j < m;){
+                int k = j + 1;
+                char ch = ans.charAt(j);
+                while (k < m && ans.charAt(k) == ch){
+                    k++;
+                }
+                int count = k - j;
+                cur += count + "" + ch;
+                j = k;
+            }
+            ans = cur;
+        }
+        return ans;
     }
 }
