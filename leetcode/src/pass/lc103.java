ab0c977766b10900bb694749567153d0e05d5476
ZBurNIngram <dumbledorebryant@gmail.com>
2022-02-05 15:30:34 -0500

    2022/02/05 a lc103

--- leetcode/src/pass/lc103.java
+++ leetcode/src/pass/lc103.java
@@ -3,7 +3,6 @@ package pass;
 import java.util.*;
 public class lc103 {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>();
         Queue<TreeNode> queue = new LinkedList<>();
 
         List<List<Integer>> ans = new ArrayList<>();
@@ -11,39 +10,31 @@ public class lc103 {
             return ans;
         }
 
-        List<Integer> list = new ArrayList<>();
-        q.add(root);
-        while (!q.isEmpty() || !queue.isEmpty()){
-            while (!q.isEmpty()){
-                TreeNode cur = q.poll();
+        queue.add(root);
+        boolean ltor = true;
+        while (!queue.isEmpty()){
+            List<Integer> level = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++){
+                TreeNode cur = queue.poll();
+
+                int num = cur.val;
+                if (ltor) {
+                    level.add(num);
+                }
+                else level.add(0, num);
+                
                 TreeNode left = cur.left;
                 TreeNode right = cur.right;
-            
-                if (right != null){
-                    queue.add(right);
-                }
                 if (left != null){
                     queue.add(left);
                 }
-                list.add(cur.val);
-            }
-            if (!list.isEmpty()) ans.add(new ArrayList<>(list));
-            list.clear();
-            while (!queue.isEmpty()){
-                TreeNode cur = queue.poll();
-                TreeNode left = cur.left;
-                TreeNode right = cur.right;
-
                 if (right != null){
-                    q.add(right);
+                    queue.add(right);
                 }
-                if (left != null){
-                    q.add(left);
-                }
-                list.add(cur.val);
             }
-            if (!list.isEmpty()) ans.add(new ArrayList<>(list));
-            list.clear();
+            ans.add(level);
+            ltor = !ltor;
         }
         return ans;
     }
--- leetcode/src/pass/weekly_contest/bwc2022_02_05/lc5987undone.java
+++ leetcode/src/pass/weekly_contest/bwc2022_02_05/lc5987undone.java
@@ -1,7 +1,7 @@
 package pass.weekly_contest.bwc2022_02_05;
 
-public class lc5987 {
+public class lc5987undone {
     public long minimumDifference(int[] nums) {
-        
+        return 1;
     }
 }
