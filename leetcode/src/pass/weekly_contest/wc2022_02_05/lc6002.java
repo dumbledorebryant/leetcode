375fb3f44833647c7ed18c4af02205d4ecd216da
ZBurNIngram <dumbledorebryant@gmail.com>
2022-02-06 14:18:09 -0500

    2022/02/06 a lc6002

--- leetcode/src/pass/weekly_contest/wc2022_02_05/lc6002.java
+++ leetcode/src/pass/weekly_contest/wc2022_02_05/lc6002.java
@@ -5,52 +5,66 @@ public class lc6002 {
     int[] ans;
     int len;
     int count = 0;
+    boolean flip = false;
     public lc6002(int size) {
         ans = new int[size];
         len = size;
     }
     
     public void fix(int idx) {
-        if (ans[idx] == 1) return;
-        else {
-            ans[idx] = 1;
-            count++;
+        if (flip){
+            if (ans[idx] == 1){
+                ans[idx] = 0;
+                count--;
+            }
         }
+        else {
+            if (ans[idx] == 0){
+                ans[idx] = 1;
+                count++;
+            }
+        }
+        
     }
     
     public void unfix(int idx) {
-        if (ans[idx] == 0) return;
-        else {
-            ans[idx] = 0;
-            count--;
+        if (flip){
+            if (ans[idx] == 0){
+                ans[idx] = 1;
+                count++;
+            }
         }
+        else {
+            if (ans[idx] == 1){
+                ans[idx] = 0;
+                count--;
+            }
+        }
+        
     }
     
     public void flip() {
-        for (int i = 0; i < len; i++){
-            ans[i] = 1 - ans[i];
-        }
-        count = len - count;
+        flip = !flip;
     }
     
     public boolean all() {
-        if (count == len) return true;
+        if (count() == len) return true;
         else return false;
     }
     
     public boolean one() {
-        if (count > 0) return true;
+        if (count() > 0) return true;
         else return false;
     }
     
     public int count() {
-        return count;
+        return flip ? len - count : count;
     }
     
     public String toString() {
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder("");
         for (int i : ans) {
-            sb.append(i);
+            sb.append(flip ? 1 - i : i);
         }
         return sb.toString();
     }
--- leetcode/src/pass/weekly_contest/wc2022_02_05/lc6003.java
+++ leetcode/src/pass/weekly_contest/wc2022_02_05/lc6003.java
@@ -2,45 +2,21 @@ package pass.weekly_contest.wc2022_02_05;
 
 public class lc6003 {
     public int minimumTime(String s) {
-        int length = s.length();
-        int[] left = new int[length];
-        int[] right = new int[length];
-        char[] chrs = s.toCharArray();
-
-        left[0] = chrs[0] == '1' ? 1 : 0;
-        for (int i = 1; i < length; i++){
-            char pre = chrs[i - 1];
-            char cur = chrs[i];
-            if (cur == '1' && pre == '1'){
-                left[i] = Math.min(2, left[i - 1]);
-            }
-            if (cur == '1' && pre == '0'){
-                left[i] = 2;
-            }
-            else {
-                left[i] = 0;
-            }
+        if (s.equals("0")) {
+            return 0;
         }
-
-        right[length - 1] = chrs[length - 1] == '1' ? 1 : 0;
-        for (int i = length - 2; i >= 0; i++){
-            char pre = chrs[i + 1];
-            char cur = chrs[i];
-            if (cur == '1' && pre == '1'){
-                left[i] = Math.min(2, left[i + 1]);
-            }
-            if (cur == '1' && pre == '0'){
-                left[i] = 2;
-            }
-            else {
-                left[i] = 0;
-            }
+        int N = s.length();
+        int[] left = new int[N], right = new int[N];
+        for (int i = 0; i < N; i++) {
+            left[i] = Math.min((i == 0 ? 0 : left[i - 1]) + (s.charAt(i) == '1' ? 2 : 0), i + 1);
         }
-
-        int cost = 0;
-        for (int i = 1; i < length; i++){
-            cost += Math.min(left[i], right[i]);
+        for (int i = N - 1; i >= 0; i--) {
+            right[i] = Math.min((i == N - 1 ? 0 : right[i + 1]) + (s.charAt(i) == '1' ? 2 : 0), N - i);
         }
-        return cost;
+        int res = N;
+        for (int i = 1; i < N; i++) {
+            res = Math.min(res, left[i - 1] + right[i]);
+        }
+        return res;
     }
 }
