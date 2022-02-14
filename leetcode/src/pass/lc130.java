d609f0b86c7c21c999c824cd5cbefcd3cd902510
ZBurNIngram <dumbledorebryant@gmail.com>
2022-02-13 23:37:07 -0500

    2022/02/13 a lc91

---
+++ leetcode/src/pass/lc130.java
@@ -0,0 +1,65 @@
+package pass;
+
+import java.util.Arrays;
+
+public class lc130 {
+    int m;
+    int n;
+    boolean[][] visited;
+    public void solve(char[][] board) {
+        m = board.length;
+        n = board[0].length;
+        visited = new boolean[m][n];
+
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                char ch = board[i][j];
+                if (ch == 'O'){
+                    if (check(i, j, board)) board[i][j] = 'X';
+                }
+            }
+        }
+    }
+    public boolean check(int i, int j, char[][] board){
+        if (i < 0 || i >= m || j < 0 || j >= n) return false;
+        char ch = board[i][j];
+        if (ch == 'X') return true;
+        if (ch == '#') return false;
+        if (visited[i][j]) return true;
+        
+        else {
+            visited[i][j] = true;
+            boolean flag = check(i + 1, j, board) 
+                            && check(i - 1, j, board)
+                            && check(i, j + 1, board)
+                            && check(i, j - 1, board);
+            visited[i][j] = false;
+            if (flag == false) board[i][j] = '#';
+            return flag;
+        }
+    }
+}
+
+
+
+[["X","O","O","X","X","X","O","X","O","O"],
+["X","O","X","X","X","X","X","X","X","X"],
+["X","X","X","X","X","X","X","X","X","X"],
+["X","X","X","X","X","X","X","X","X","O"],
+["O","X","X","X","X","X","X","X","X","X"],
+["X","X","X","X","X","X","X","X","X","X"],
+["O","X","X","X","X","X","X","X","X","O"],
+["O","X","X","X","X","X","X","X","X","X"],
+["X","X","X","X","X","X","X","X","O","O"],
+["X","X","X","O","O","X","O","X","X","O"]]
+
+[["X","O","O","X","X","X","O","X","O","O"],
+["X","X","X","X","X","X","X","X","X","X"],
+["X","X","X","X","X","X","X","X","X","X"],
+["X","X","X","X","X","X","X","X","X","O"],
+["O","X","X","X","X","X","X","X","X","X"],
+["X","X","X","X","X","X","X","X","X","X"],
+["O","X","X","X","X","X","X","X","X","O"],
+["O","X","X","X","X","X","X","X","X","X"],
+["X","X","X","X","X","X","X","X","O","O"],
+["X","X","X","O","O","X","O","X","X","O"]]
--- leetcode/src/pass/lc91.java
+++ leetcode/src/pass/lc91.java
@@ -3,7 +3,22 @@ package pass;
 public class lc91 {
     public int numDecodings(String s) {
         int length = s.length();
+        s = " " + s;
+        int[] dp = new int[length + 1];
+        char[] chrs = s.toCharArray();
+        dp[0] = 1;
+        for (int i = 1; i <= length; i++){
+            int a = chrs[i] - '0';
+            int b = a + (chrs[i - 1] - '0') * 10;
+            if (a != 0){
+                dp[i] = dp[i - 1];
+            }
+
+            if (b <= 26 && b >= 10) {
+                dp[i] += dp[i - 2];
+            }
+        }
         
-        return count;
+        return dp[length];
     }
 }
