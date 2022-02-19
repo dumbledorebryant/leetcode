2bf10333eaa8ab818c033215bbd792f7a889127b
ZBurNIngram <dumbledorebryant@gmail.com>
2022-02-19 00:01:10 -0500

    2022/02/18 e lc130

--- leetcode/src/pass/lc130.java
+++ leetcode/src/pass/lc130.java
@@ -1,41 +1,37 @@
 package pass;
 
-import java.util.Arrays;
-
 public class lc130 {
     int m;
     int n;
-    boolean[][] visited;
     public void solve(char[][] board) {
         m = board.length;
         n = board[0].length;
-        visited = new boolean[m][n];
-
+        for (int i = 0; i < n; i++){
+            if (board[0][i] == 'O') dfs(board, 0, i);
+            if (board[m - 1][i] == 'O') dfs(board, m - 1, i);
+        }
+        for (int i = 1; i < m - 1; i++){
+            if (board[i][0] == 'O') dfs(board, i, 0);
+            if (board[i][n - 1] == 'O') dfs(board, i, n - 1);
+        }
         for (int i = 0; i < m; i++){
             for (int j = 0; j < n; j++){
-                char ch = board[i][j];
-                if (ch == 'O'){
-                    if (check(i, j, board)) board[i][j] = 'X';
-                }
+                if (board[i][j] == 'O') board[i][j] = 'X';
+                if (board[i][j] == '#') board[i][j] = 'O';
             }
         }
-    }
-    public boolean check(int i, int j, char[][] board){
-        if (i < 0 || i >= m || j < 0 || j >= n) return false;
-        char ch = board[i][j];
-        if (ch == 'X') return true;
-        if (ch == '#') return false;
-        if (visited[i][j]) return true;
         
-        else {
-            visited[i][j] = true;
-            boolean flag = check(i + 1, j, board) 
-                            && check(i - 1, j, board)
-                            && check(i, j + 1, board)
-                            && check(i, j - 1, board);
-            visited[i][j] = false;
-            if (flag == false) board[i][j] = '#';
-            return flag;
-        }
+    }
+
+    public void dfs(char[][] board, int i, int j){
+        if (i < 0 || i >= m || j < 0 || j >= n) return;
+        if (board[i][j] == 'X') return;
+        if (board[i][j] == '#') return;
+        
+        board[i][j] = '#';
+        dfs(board, i + 1, j);
+        dfs(board, i - 1, j);
+        dfs(board, i, j + 1);
+        dfs(board, i, j - 1);
     }
 }
