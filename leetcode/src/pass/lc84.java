9a3772a5e2df5eddcdc3f1dc2a69d09cae2c11e3
ZBurNIngram <dumbledorebryant@gmail.com>
2022-02-18 23:12:12 -0500

    2022/02/18 e lc84

--- leetcode/src/pass/lc84.java
+++ leetcode/src/pass/lc84.java
@@ -4,15 +4,27 @@ import java.util.Stack;
 
 public class lc84 {
     public int largestRectangleArea(int[] heights) {
-        int ans = 0;
-        int length = heights.length;
         Stack<Integer> stack = new Stack<>();
-        stack.add(heights[0]);
-        for (int i = 1; i < length; i++){
+        stack.push(0);
+        
+        int ans = 0;
+
+        int length = heights.length;
+        int[] nheights = new int[length + 2];
+        for (int i = 1; i <= length; i++){
+            nheights[i] = heights[i - 1];
+        }
+        heights = nheights;
+        for (int i = 1; i < heights.length; i++){
             int num = heights[i];
-            while (!stack.isEmpty() && stack.peek() > num){
-                
+            while (!stack.isEmpty() && heights[stack.peek()] > num){
+                int mid = stack.pop();
+                int left = stack.peek();
+                int right = i;
+                int height = heights[mid];
+                ans = Math.max(ans, (right - left - 1) * height);
             }
+            stack.push(i);
         }
         return ans;
     }
