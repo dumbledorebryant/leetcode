cba68b452715147a197924876f9a71d228cead76
ZBurNIngram <dumbledorebryant@gmail.com>
2022-02-18 20:19:20 -0500

    2022/02/18 e lc78

--- leetcode/src/pass/lc78.java
+++ leetcode/src/pass/lc78.java
@@ -1,8 +1,24 @@
 package pass;
 import java.util.*;
 public class lc78 {
+    List<List<Integer>> ans;
     public List<List<Integer>> subsets(int[] nums) {
-        int length = nums.length;
+        ans = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        construct(nums, 0, list);
+        return ans;
     }
-    public List<Integer> construct()
+    public void construct(int[] nums, int i, List<Integer> cur){
+        int length = nums.length;
+        if (i >= length) {
+            ans.add(new ArrayList<>(cur));
+        }
+        else {
+            int num = nums[i];
+            construct(nums, i + 1, cur);
+            cur.add(num);
+            construct(nums, i + 1, cur);
+            cur.remove(cur.size() - 1);
+        }
+    }
 }
