e3b14936299061bfece2c7d52f10bd0af8ccbf33
ZBurNIngram <dumbledorebryant@gmail.com>
2022-02-19 09:40:54 -0500

    2022/02/18 e lc122

--- leetcode/src/pass/lc122.java
+++ leetcode/src/pass/lc122.java
@@ -3,6 +3,14 @@ package pass;
 public class lc122 {
     public int maxProfit(int[] prices) {
         int length = prices.length;
-        
+        int[][] dp = new int[length][2];
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+        for (int i = 1; i < length; i++){
+            int num = prices[i];
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - num);
+            dp[i][1] = Math.max(dp[i - 1][0] + num, dp[i - 1][1]);
+        }
+        return dp[length - 1][1];
     }
 }
