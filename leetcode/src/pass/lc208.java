af215547d9b914564073649cd5b61ff86840d239
ZBurNIngram <dumbledorebryant@gmail.com>
2022-02-21 09:54:56 -0500

    2022/02/21 m lc208

--- leetcode/src/pass/lc208.java
+++ leetcode/src/pass/lc208.java
@@ -4,25 +4,51 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class lc208 {
-    Set<String> sset = new HashSet<>();
-    Set<String> pset = new HashSet<>();
+    boolean isEnd;
+    lc208[] children = new lc208[26];
     public lc208() {
-        
+
     }
     
     public void insert(String word) {
-        sset.add(word);
+        lc208 node = this;
         int length = word.length();
-        for (int i = 1; i <= length; i++){
-            pset.add(word.substring(0, i));
+        for (int i = 0; i < length; i++){
+            int ch = word.charAt(i) - 'a';
+            if (node.children[ch] != null){
+                continue;
+            }
+            else {
+                node.children[ch] = new lc208();
+            }
+            node = node.children[ch];
         }
+        node.isEnd = true;
     }
     
     public boolean search(String word) {
-        return sset.contains(word);
+        lc208 node = this;
+        int length = word.length();
+        for (int i = 0; i < length; i++){
+            int ch = word.charAt(i) - 'a';
+            if (node.children[ch] == null) return false;
+            else {
+                node = node.children[ch];
+            }
+        }
+        return node != null && node.isEnd;
     }
     
     public boolean startsWith(String prefix) {
-        return pset.contains(prefix);
+        lc208 node = this;
+        int length = prefix.length();
+        for (int i = 0; i < length; i++){
+            int ch = prefix.charAt(i) - 'a';
+            if (node.children[ch] == null) return false;
+            else {
+                node = node.children[ch];
+            }
+        }
+        return node != null;
     }
 }
