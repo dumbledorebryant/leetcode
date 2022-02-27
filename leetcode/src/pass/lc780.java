aa7939b750d0fee51e20a0e31fd4ea3d9520776f
ZBurNIngram <dumbledorebryant@gmail.com>
2022-02-26 23:29:41 -0500

    2022/02/26 e lc1641

--- leetcode/src/pass/lc780.java
+++ leetcode/src/pass/lc780.java
@@ -2,6 +2,8 @@ package pass;
 
 public class lc780 {
     public boolean reachingPoints(int sx, int sy, int tx, int ty) {
-        while (tx > sx && ty > )
+        while (tx > sx && ty > sy){
+            
+        }
     }
 }
