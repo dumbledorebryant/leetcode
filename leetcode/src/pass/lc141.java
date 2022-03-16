b3201cbc1186acfe299a668c7028b27558fe8790
ZBurNIngram <dumbledorebryant@gmail.com>
2022-03-16 10:44:31 -0400

    2022/03/16 m lc141

--- leetcode/src/pass/lc141.java
+++ leetcode/src/pass/lc141.java
@@ -13,4 +13,15 @@ public class lc141 {
         }
         return false;
     }
+
+    public boolean hasCycle2(ListNode head) {
+        ListNode a = head;
+        ListNode b = head;
+        while (b != null && b.next != null){
+            a = a.next;
+            b = b.next.next;
+            if (a == b) return true;
+        }
+        return false;
+    }
 }
