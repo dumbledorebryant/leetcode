41dfc933340bbb0ac75140e35d0d35f623db5ab8
ZBurNIngram <dumbledorebryant@gmail.com>
2022-03-16 09:28:58 -0400

    2022/03/16 m lc287

--- leetcode/src/pass/lc287.java
+++ leetcode/src/pass/lc287.java
@@ -2,6 +2,20 @@ package pass;
 
 public class lc287 {
     public int findDuplicate(int[] nums) {
-        
+        int length = nums.length;
+        int ans = 0;
+        for (int i = 0; i < 32; i++){
+            int bit = 0;
+            int ori = 0;
+            for (int j = 0; j < length; j++){
+                int num = nums[j];
+                bit += ((num >> i) & 1);
+                if (j >= 1) ori += (((j + 1) >> i) & 1);
+            }
+            if (bit > ori){
+                ans |= (1 << i);
+            }
+        }
+        return ans;
     }
 }
