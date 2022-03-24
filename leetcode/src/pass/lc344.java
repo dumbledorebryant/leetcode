b36adf99175da66476936e561580a53f6b4dfd4e
ZBurNIngram <dumbledorebryant@gmail.com>
2022-03-24 15:59:17 -0400

    2022/03/24 a lc344

--- leetcode/src/pass/lc344.java
+++ leetcode/src/pass/lc344.java
@@ -1,5 +1,19 @@
 package pass;
 
 public class lc344 {
-    
+    public void reverseString(char[] s) {
+        int length = s.length;
+        int i = 0; 
+        int j = length - 1;
+        while (i < j){
+            swap(s, i, j);
+            i++;
+            j--;
+        }
+    }
+    public void swap(char[] s, int i, int j){
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+    }
 }
