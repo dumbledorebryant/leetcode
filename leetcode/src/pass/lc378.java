8350b1d415c9c23932df249ae5cb7128690288e3
ZBurNIngram <dumbledorebryant@gmail.com>
2022-04-06 15:36:35 -0400

    2022/04/06 a lc380

---
+++ leetcode/src/pass/lc378.java
@@ -0,0 +1,10 @@
+package pass;
+import java.util.*;
+public class lc378 {
+    public int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;
+        
+        List<Integer> list = new ArrayList<>();  
+        list.      
+    }
+}
