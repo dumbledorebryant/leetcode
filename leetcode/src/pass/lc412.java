348d86e2ec060f6626ceaf68f98cc9d33f0f9e43
ZBurNIngram <dumbledorebryant@gmail.com>
2022-04-07 15:23:03 -0400

    2022/04/07 a lc412

---
+++ leetcode/src/pass/lc412.java
@@ -0,0 +1,21 @@
+package pass;
+import java.util.*;
+public class lc412 {
+    public List<String> fizzBuzz(int n) {
+        List<String> ans = new ArrayList<>();
+        for (int i = 1; i <= n; i++){
+            if (i % 3 == 0 && i % 5 == 0){
+                ans.add("FizzBuzz");
+            }
+            else if (i % 3 == 0){
+                ans.add("Fizz");
+            }
+            else if (i % 5 == 0){
+                ans.add("Buzz");
+            }
+            
+            else ans.add(String.valueOf(i));
+        }
+        return ans;
+    }
+}
