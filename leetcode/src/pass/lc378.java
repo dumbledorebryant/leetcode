68ab2edf1f328a5a45a44df1ed99b66a7132743c
ZBurNIngram <dumbledorebryant@gmail.com>
2022-04-10 23:39:47 -0400

    2022/04/10 e lc378

--- leetcode/src/pass/lc378.java
+++ leetcode/src/pass/lc378.java
@@ -1,10 +1,35 @@
 package pass;
 import java.util.*;
 public class lc378 {
-    public int kthSmallest(int[][] matrix, int k) {
+    public int kthSmallest(int[][] matrix, int k) {        
         int n = matrix.length;
-        
-        List<Integer> list = new ArrayList<>();  
-        list.      
+        int left = matrix[0][0];
+        int right = matrix[n - 1][n - 1];
+        while (left < right){
+            int mid = left + ((right - left) >> 1);
+            if (check(matrix, mid, k, n)){
+                right = mid;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+    public boolean check(int[][] matrix, int mid, int k, int n){
+        int i = n - 1;
+        int j = 0;
+        int cnt = 0;
+        while (i >= 0 && j < n){
+            int num = matrix[i][j];
+            if (num <= mid){
+                cnt += (i + 1); 
+                j++;
+            } 
+            else {
+                i--;
+            }
+        }
+        return cnt >= k;
     }
 }
