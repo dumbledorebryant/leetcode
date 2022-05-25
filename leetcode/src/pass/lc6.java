9c54a96e97f350070d7ef3648b275aabb2651384
ZBurNIngram <dumbledorebryant@gmail.com>
2022-05-25 09:58:58 -0400

    lc 6 done

--- leetcode/src/pass/lc6.java
+++ leetcode/src/pass/lc6.java
@@ -1,5 +1,28 @@
 package pass;
 
+import java.util.*;
+
 public class lc6 {
-    
+    public String convert(String s, int numRows) {
+        if (numRows == 1) return s;
+        List<StringBuilder> list = new ArrayList<>();
+        for (int i = 0; i < numRows; i++){
+            list.add(new StringBuilder());
+        }
+        int length = s.length();
+        int j = 0;
+        int flag = 1;
+        for (int i = 0; i < length; i++){
+            list.get(j).append(s.charAt(i));
+            j += flag;
+            if (j == 0 || j == numRows - 1){
+                flag = -flag;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < numRows; i++){
+            sb.append(list.get(i));
+        }
+        return sb.toString();
+    }
 }
