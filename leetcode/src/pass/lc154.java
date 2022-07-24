35d91a94d7c695f93ec04ca4c324d980269170d9
ZBurNIngram <dumbledorebryant@gmail.com>
2022-07-23 23:28:34 -0400

    lc 154 finished

---
+++ leetcode/src/pass/lc154.java
@@ -0,0 +1,32 @@
+package pass;
+
+public class lc154 {
+    public int findMin(int[] nums) {
+        int length = nums.length;
+        return binarySearch(nums, 0, length - 1);
+    }
+    public int binarySearch(int[] nums, int i, int j){
+        if (i == j) return nums[i];
+        int start = nums[i];
+        int end = nums[j];
+        if (j - i == 1) return Math.min(start, end);
+        int mid = i + ((j - i) >> 1);
+        int num = nums[mid];
+        
+        if (num > end) {
+            return binarySearch(nums, mid, j);
+        }
+        else if (num < start) {
+            return binarySearch(nums, i, mid);
+        }
+        else {
+            while (nums[i] == mid){
+                i++;
+            }
+            while (nums[j] == mid){
+                j--;
+            }
+            return binarySearch(nums, i, j)
+        }
+    }
+}
