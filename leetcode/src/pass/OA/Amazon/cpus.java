94f95d0ca4cb735004919990fa302b4ec9561e25
ZBurNIngram <dumbledorebryant@gmail.com>
2022-08-13 10:43:52 -0400

    Amazon OA add xyz and cpu

--- leetcode/src/pass/OA/Amazon/cpus.java
+++ leetcode/src/pass/OA/Amazon/cpus.java
@@ -1,5 +1,19 @@
 package pass.OA.Amazon;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class cpus {
-    
+    public int cpuProcessor(int[] arr){
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : arr){
+            map.put(i, i);
+        }
+        int sum = 0;
+        for (int i : arr){
+            sum += map.get(i);
+            map.put(i, (int)Math.ceil(map.get(i) / 2.0));
+        }
+        return sum;
+    }
 }
