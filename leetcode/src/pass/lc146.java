a257afd60116ffe40573e88ab163234ea945b54b
ZBurNIngram <dumbledorebryant@gmail.com>
2022-09-01 00:02:01 -0400

    lc146 finished

--- leetcode/src/pass/lc146.java
+++ leetcode/src/pass/lc146.java
@@ -3,29 +3,32 @@ package pass;
 import java.util.*;
 
 public class lc146 {
-    Map<Integer, Integer> map = new HashMap<>();
-    Deque<Integer> dq = new LinkedList<>();
-    List<Integer> array = new ArrayList<>();
+    LinkedHashMap<Integer, Integer> cache = new LinkedHashMap<>();
     int cap;
     public lc146(int capacity) {
         cap = capacity;
     }
     
     public int get(int key) {
-        if (map.containsKey(key)) {
-            dq.push(key);
-            return map.get(key);
+        if (cache.containsKey(key)) {
+            int value = cache.get(key);
+            cache.remove(key);
+            cache.put(key, value);
+            return value;
         }
         else return -1;
     }
     
     public void put(int key, int value) {
-        map.put(key, value);
-        if (map.size() > cap){
-            dq.
-            while (map.containsKey(dq.removeFirst())){
-                map.remove(key)
-            }
+        if (cache.containsKey(key)){
+            cache.remove(key);
+            cache.put(key, value);
+        }
+        else {
+            
+        }
+        if (size == cap){
+            cache.remove(cache.keySet().iterator().next());
         }
     }
 }
