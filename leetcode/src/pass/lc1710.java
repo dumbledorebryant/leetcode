9ceae337395e2098eb26e318a7849bc1a85ed923
ZBurNIngram <dumbledorebryant@gmail.com>
2022-09-03 17:26:37 -0400

    lc1710 updated finished

---
+++ leetcode/src/pass/lc1710.java
@@ -0,0 +1,25 @@
+package pass;
+import java.util.*;
+public class lc1710 {
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+        Arrays.sort(boxTypes, (a, b) -> b[1] - a[1]);
+        int ans = 0;
+        int i = 0;
+        int length = boxTypes.length;
+        while (truckSize > 0 && i < length){
+            int num = boxTypes[i][0];
+            int unit = boxTypes[i][1];
+            if (truckSize > num){
+                truckSize -= num;
+                ans += (num * unit);
+            }
+            else {
+                
+                ans += (truckSize * unit);
+                truckSize = 0;
+            }
+            i++;
+        }
+        return ans;
+    }
+}
