3124aeeaef68d235947e2771ffa7a8a80465cd81
ZBurNIngram <dumbledorebryant@gmail.com>
2022-10-07 21:48:08 -0400

    Doordash filesystem finished

--- leetcode/src/pass/Company/Doordash/VO/FileSystem.java
+++ leetcode/src/pass/Company/Doordash/VO/FileSystem.java
@@ -5,26 +5,70 @@ public class FileSystem {
         String name;
         int value;
         Map<String, Node> children;
+
+        public Node(String file, int val){
+            name = file;
+            value = val;
+            children = new HashMap<>();
+        }
     }
     Node root;
     public FileSystem() {
-        root = new Node();
-        root.value = Integer.MAX_VALUE;
+        root = new Node("", 0);
     }
     
     public boolean createPath(String path, int value) {
-        String[] list = path.split("/");
         Node ptr = root;
+        String[] list = path.split("/");
         int len = list.length;
-        for (String node : list){
-            if (root.children.containsKey(node)){
-                ptr = ptr.children.get(node);
+        for (int i = 0; i < len; i++){
+            String node = list[i];
+            if (i != len - 1){
+                if (ptr.children.containsKey(node)){
+                    ptr = ptr.children.get(node);
+                }
+                else return false;
             }
-            else 
+            else {
+                Node temp = new Node(node, value);
+                ptr.children.put(node, temp);
+            }
         }
+        return true;
     }
     
     public int get(String path) {
-        
+        String[] list = path.split("/");
+        Node ptr = root;
+        int len = list.length;
+        for (int i = 1; i < len; i++){
+            String node = list[i];
+            if (ptr.children.containsKey(node)){
+                ptr = ptr.children.get(node);
+            }
+            else {
+                return -1;
+            }
+        }
+        return ptr.value;
+    }
+
+    public void delete(String path){
+        String[] list = path.split("/");
+        Node ptr = root;
+        int len = list.length;
+        for (int i = 1; i < len; i++){
+            String node = list[i];
+            if (ptr.children.containsKey(node)){
+                if (i == len - 1){
+                    ptr.children.remove(node);
+                    return;
+                }
+                ptr = ptr.children.get(node);
+            }
+            else {
+                return;
+            }
+        }
     }
 }
