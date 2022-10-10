301856f6bedc9e119b4410a7655c24c14120f8f1
ZBurNIngram <dumbledorebryant@gmail.com>
2022-10-10 15:42:51 -0400

    lc299 finished

--- leetcode/src/pass/lc299.java
+++ leetcode/src/pass/lc299.java
@@ -1,7 +1,42 @@
 package pass;
 
+import java.util.*;
+
 public class lc299 {
     public String getHint(String secret, String guess) {
-        
+        char[] s = secret.toCharArray();
+        char[] g = guess.toCharArray();
+        Map<Character, Integer> count = new HashMap<>();
+        for (char ch : s){
+            count.put(ch, count.getOrDefault(ch, 0) + 1);
+        }
+        int length = g.length;
+        int bull = 0;
+        int cow = 0;
+        for (int i = 0; i < length; i++){
+            char ch = g[i];
+            if (ch == secret.charAt(i)) {
+                bull++;
+                count.put(ch, count.get(ch) - 1);
+            }
+            else {
+                if (count.containsKey(ch)){
+                    count.put(ch, count.get(ch) - 1);
+                    cow++;
+                }
+            }
+        }
+        for (char key : count.keySet()){
+            int num = count.get(key);
+            if (num < 0) {
+                cow += num;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(bull);
+        sb.append("A");
+        sb.append(cow);
+        sb.append("B");
+        return sb.toString();
     }
 }
