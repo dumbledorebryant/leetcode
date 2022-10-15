8db9fcee8c4f91d6ab5e9d67ccf5e866a4b64c71
ZBurNIngram <dumbledorebryant@gmail.com>
2022-10-15 15:22:20 -0400

    lc6 finished

--- leetcode/src/pass/lc6.java
+++ leetcode/src/pass/lc6.java
@@ -16,12 +16,36 @@ public class lc6 {
             list.get(j).append(s.charAt(i));
             j += flag;
             if (j == 0 || j == numRows - 1){
-                flag = -flag;
+                flag = - flag;
             }
         }
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < numRows; i++){
             sb.append(list.get(i));
+        }
+        return sb.toString();
+    }
+
+    public String zigzag(String s, int numRows){
+        if (numRows == 1) return s;
+        List<StringBuilder> list = new ArrayList<>();
+        for (int i = 0; i < numRows; i++){
+            list.add(new StringBuilder());
+        }
+        int j = 0;
+        int flag = -1;
+        int length = s.length();
+        for (int i = 0; i < length; i++){
+            char ch = s.charAt(i);
+            list.get(j).append(ch);
+            if (j == 0 || j == numRows - 1){
+                flag = - flag;
+            }
+            j += flag;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (StringBuilder sbd : list){
+            sb.append(sbd);
         }
         return sb.toString();
     }
