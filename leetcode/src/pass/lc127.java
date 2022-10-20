0db191ce72e8d3e5b99704d8a92f10fac9d257b0
ZBurNIngram <dumbledorebryant@gmail.com>
2022-10-19 21:21:13 -0400

    lc127 finished

--- leetcode/src/pass/lc127.java
+++ leetcode/src/pass/lc127.java
@@ -32,4 +32,38 @@ public class lc127 {
         }
         return 0;
     }
+
+    public int ladderSize(String beginWord, String endWord, List<String> wordList){
+        Deque<String> queue = new ArrayDeque<>();
+        Set<String> set = new HashSet<>(wordList);
+        queue.add(beginWord);
+        int steps = 1;
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            for (int i = 0; i < size; i++){
+                String cur = queue.removeFirst();
+                if (cur.equals(endWord)){
+                    return steps;
+                }
+
+                char[] chrs = cur.toCharArray();
+                int len = chrs.length;
+                for (int j = 0; j < len; j++){
+                    char ori = chrs[j];
+                    for (char ch = 'a'; ch <= 'z'; ch++){
+                        chrs[j] = ch;
+                        String str = String.valueOf(chrs);
+                        if (set.contains(str)){
+                            queue.addLast(str);
+                            set.remove(str);
+                        }
+                    }
+                    chrs[j] = ori;
+                }
+                
+            }
+            steps++;
+        }
+        return 0;
+    }
 }
