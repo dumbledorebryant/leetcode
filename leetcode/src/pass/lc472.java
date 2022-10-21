f5c4bdd72a5a1f4ba9e6e900c849891f80be3e2a
ZBurNIngram <dumbledorebryant@gmail.com>
2022-10-21 11:04:24 -0400

    lc472 finished

--- leetcode/src/pass/lc472.java
+++ leetcode/src/pass/lc472.java
@@ -58,3 +58,58 @@ public class lc472 {
         }
     }
 }
+class Solution {
+    class Trie{
+        boolean isEnd;
+        Trie[] children;
+        public Trie(){
+            isEnd = false;
+            children = new Trie[26];
+        }
+    }
+    Trie root = new Trie();
+
+    public void insert(String word){
+        char[] chrs = word.toCharArray();
+
+        Trie cur = root;
+        for (char ch : chrs){
+            int index = ch - 'a';
+            if (cur.children[index] == null){
+                cur.children[index] = new Trie();
+            }
+            cur = cur.children[index];
+        }
+        cur.isEnd = true;
+    }
+
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+        List<String> ans = new ArrayList<>();
+        for (String word : words){
+            if (dfs(word, 0)){
+                ans.add(word);
+            }
+            else insert(word);
+        }
+        return ans;
+    }
+
+    public boolean dfs(String word, int idx){
+        char[] chrs = word.toCharArray();
+        int length = word.length();
+        if (idx == length) return true;
+
+        Trie cur = root;
+        for (int i = idx; i < length; i++){
+            char ch = chrs[i];
+            int index = ch - 'a';
+            if (cur.children[index] == null) return false;
+            cur = cur.children[index];
+            if (cur.isEnd){
+                if (dfs(word, i + 1)) return true;
+            }
+        }
+        return false;
+    }
+}
