7fd04a0a17184662384422291c56da241a63a6fc
ZBurNIngram <dumbledorebryant@gmail.com>
2022-10-26 10:55:11 -0400

    lc304 finished

---
+++ leetcode/src/pass/lc304.java
@@ -0,0 +1,19 @@
+package pass;
+
+public class lc304 {
+    int[][] prefix;
+    public lc304(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        prefix = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++){
+            for (int j = 1; j <= n; j++){
+                prefix[i][j] = prefix[i - 1][j] + prefix[i][j - 1] - prefix[i - 1][j - 1] + matrix[i - 1][j - 1];
+            }
+        }
+    }
+    
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return prefix[row2 + 1][col2 + 1] + prefix[row1][col1] - prefix[row2 + 1][col1] - prefix[row1][col2 + 1];
+    }
+}
