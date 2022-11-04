237a06610b55218b63b9aeaebab79285d977859c
ZBurNIngram <dumbledorebryant@gmail.com>
2022-11-04 10:48:39 -0400

    lc121 finished

--- leetcode/src/pass/lc121.java
+++ leetcode/src/pass/lc121.java
@@ -4,7 +4,7 @@ public class lc121 {
     public int maxProfit(int[] prices) {
         int n = prices.length;
         int[][] dp = new int[n + 1][2];
-        dp[0][1] = -prices[0];
+        dp[0][1] = Integer.MIN_VALUE;
         for (int i = 1; i <= n; i++){
             dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i - 1]);
             dp[i][1] = Math.max(dp[i - 1][1], -prices[i - 1]);
