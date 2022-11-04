3c1b28a3ff5f6b2d4fcdbdcc09d44d7569ce2f15
ZBurNIngram <dumbledorebryant@gmail.com>
2022-11-04 09:55:54 -0400

    lc877 finished

--- leetcode/src/pass/lc877.java
+++ leetcode/src/pass/lc877.java
@@ -2,6 +2,43 @@ package pass;
 
 public class lc877 {
     public boolean stoneGame(int[] piles) {
-        return true;
+        class Pair{
+            int fir, sec;
+            Pair(int first, int second){
+                fir = first;
+                sec = second;
+            }
+        }
+
+        int n = piles.length;
+        Pair[][] dp = new Pair[n][n];
+        for (int i = 0; i < n; i++){
+            for (int j = i; j < n; j++){
+                dp[i][j] = new Pair(0, 0);
+            }
+        }
+
+        for (int i = 0; i < n; i++){
+            dp[i][i].fir = piles[i];
+            dp[i][i].sec = 0;
+        }
+
+        for (int i = n - 2; i >= 0; i--){
+            for (int j = i + 1; j < n; j++){
+                int l = piles[i] + dp[i + 1][j].sec;
+                int r = piles[j] + dp[i][j - 1].sec;
+                
+                if (l > r){
+                    dp[i][j].fir = l;
+                    dp[i][j].sec = dp[i + 1][j].fir;
+                }
+                else {
+                    dp[i][j].fir = r;
+                    dp[i][j].sec = dp[i][j - 1].fir;
+                }
+            }
+        }
+        Pair ans = dp[0][n - 1];
+        return (ans.fir - ans.sec) >= 0;
     }
 }
