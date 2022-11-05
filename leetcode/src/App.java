5dd90718bd5cd777abcdc4942c8727c4b52d994b
ZBurNIngram <dumbledorebryant@gmail.com>
2022-11-05 14:58:30 -0400

    lc468 finished

--- leetcode/src/App.java
+++ leetcode/src/App.java
@@ -1,12 +1,20 @@
-import java.util.Scanner;
-
-import pass.weekly_contest.wc2022_01_29.lc5994Test;
-
 public class App {
     public static void main(String[] args) throws Exception 
     {
-        lc5994Test test = new lc5994Test();
-        test.testSubStrHash();
-        System.out.println("shit");
+        System.out.println("Hi Zbn");
+    }
+}
+
+abstract class a{
+    protected void aa(){};
+    //public abstract void bb();
+    //protected abstract void cc();
+    void dd(){}
+}
+
+class b extends a{
+    @Override
+    public void aa(){
+           
     }
 }
--- leetcode/src/pass/lc468.java
+++ leetcode/src/pass/lc468.java
@@ -2,24 +2,45 @@ package pass;
 
 public class lc468 {
     public String validIPAddress(String queryIP) {
-        String[] nums = queryIP.split("\\.", -1);
-        for (String x : nums) {
-            // Validate integer in range (0, 255):
+        if (queryIP.split(":").length == 1){
+            String[] nums = queryIP.split("\\.", -1);
+            if (nums.length != 4) return "Neither";
+            for (String x : nums) {
+                // Validate integer in range (0, 255):
             
-            // 1. length of chunk is between 1 and 3
-            if (x.length() == 0 || x.length() > 3) return "Neither";
+                // 1. length of chunk is between 1 and 3
+                if (x.length() == 0 || x.length() > 3) return "Neither";
             
-            // 2. no extra leading zeros
-            if (x.charAt(0) == '0' && x.length() != 1) return "Neither";
+                // 2. no extra leading zeros
+                if (x.charAt(0) == '0' && x.length() != 1) return "Neither";
       
-            // 3. only digits are allowed
-            for (char ch : x.toCharArray()) {
-                if (! Character.isDigit(ch)) return "Neither";
-            }
+                // 3. only digits are allowed
+                for (char ch : x.toCharArray()) {
+                    if (! Character.isDigit(ch)) return "Neither";
+                }
             
-            // 4. less than 255
-            if (Integer.parseInt(x) > 255) return "Neither";
+                // 4. less than 255
+                if (Integer.parseInt(x) > 255) return "Neither";
+            }
+            return "IPv4";
         }
-        return "IPv4";
+        else {
+            String[] words = queryIP.split(":");
+            if (words.length != 8) return "Neither";
+            if (queryIP.charAt(queryIP.length() - 1) == ':') return "Neither";
+            for (String x : words){
+                if (x.length() == 0 || x.length() > 4) return "Neither";
+                for (char ch : x.toCharArray()){
+                    if (!Character.isDigit(ch)){
+                        if (Character.isAlphabetic(ch)){
+                            if ((ch >= 'a' && ch <= 'f') || (ch >= 'A' && ch <= 'F')){}
+                            else return "Neither";
+                        }
+                        else return "Neither";
+                    }
+                }
+            }
+            return "IPv6";
+        }
     }
 }
