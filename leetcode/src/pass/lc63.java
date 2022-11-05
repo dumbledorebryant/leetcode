2be103103281cad9f273b1026e75e3db269a9189
ZBurNIngram <dumbledorebryant@gmail.com>
2022-11-05 15:56:13 -0400

    lc63 finished

---
+++ leetcode/src/pass/lc63.java
@@ -0,0 +1,34 @@
+package pass;
+
+public class lc63 {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        if (obstacleGrid[m - 1][n - 1] == 1) return 0;
+        if (obstacleGrid[0][0] == 1) return 0;
+        
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for (int i = 1; i < m; i++){
+            if (obstacleGrid[i][0] != 1)
+                dp[i][0] = dp[i - 1][0];
+            else dp[i][0] = 0;
+        }
+        for (int i = 1; i < n; i++){
+            if (obstacleGrid[0][i] != 1)
+                dp[0][i] = dp[0][i - 1];
+            else dp[0][i] = 0;
+        }
+        for (int i = 1; i < m; i++){
+            for (int j = 1; j < n; j++){
+                if (obstacleGrid[i - 1][j] != 1)
+                    dp[i][j] += dp[i - 1][j];
+                if (obstacleGrid[i][j - 1] != 1)
+                    dp[i][j] += dp[i][j - 1];
+            }
+        }
+        
+        return dp[m - 1][n - 1];
+    }
+}
