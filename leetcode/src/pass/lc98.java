26801054ae0a8179874c54c2669cf826d4630f86
ZBurNIngram <dumbledorebryant@gmail.com>
2022-11-09 10:36:37 -0500

    lc98 finished

--- leetcode/src/pass/lc98.java
+++ leetcode/src/pass/lc98.java
@@ -1,33 +1,19 @@
 package pass;
 
-import java.beans.Transient;
-import java.util.*;
-
 public class lc98 {
     public boolean isValidBST(TreeNode root) {
-        if (root == null) return true;
-        else {
-            List<Integer> lp = isBST(root.left);
-            List<Integer> rp = isBST(root.right);
-            int num = root.val;
-            if (rp != null && num >= rp.get(0)) return false;
-            if (lp != null && num <= lp.get(1)) return false;
-            return isValidBST(root.left) && isValidBST(root.right);
-        }
+        return isValidBst(root, null, null);
     }
-    public List<Integer> isBST(TreeNode root) {
-        if (root == null) return null;
-        int min, max;
-        List<Integer> pair = new ArrayList<>();
-        List<Integer> lp = isBST(root.left);
-        List<Integer> rp = isBST(root.right);
-        int num = root.val;
-        if (lp != null) min = lp.get(0);
-        else min = num;
-        if (rp != null) max = rp.get(1);
-        else max = num;
-        pair.add(min);
-        pair.add(max);
-        return pair;
+
+    /* 限定以 root 为根的子树节点必须满足 max.val > root.val > min.val */
+    boolean isValidBst(TreeNode root, TreeNode min, TreeNode max) {
+        // base case
+        if (root == null) return true;
+        // 若 root.val 不符合 max 和 min 的限制，说明不是合法 BST
+        if (min != null && root.val <= min.val) return false;
+        if (max != null && root.val >= max.val) return false;
+        // 限定左子树的最大值是 root.val，右子树的最小值是 root.val
+        return isValidBst(root.left, min, root) 
+            && isValidBst(root.right, root, max);   
     }
 }
