e51e506ecce4693c71df9d3b516fc0550de1a99b
ZBurNIngram <dumbledorebryant@gmail.com>
2022-11-20 16:13:09 -0500

    lc263 finished

---
+++ leetcode/src/pass/lc263.java
@@ -0,0 +1,11 @@
+package pass;
+
+public class lc263 {
+    public boolean isUgly(int n) {
+        if (n <= n) return false;
+        while (n % 2 == 0) n /= 2;
+        while (n % 3 == 0) n /= 3;
+        while (n % 5 == 0) n /= 5;
+        return n == 1;
+    }
+}
