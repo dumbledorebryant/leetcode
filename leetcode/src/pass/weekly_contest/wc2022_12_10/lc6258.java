d31a8638070ca71f2fddccab770e36aa7a070ff8
ZBurNIngram <dumbledorebryant@gmail.com>
2022-12-13 11:42:42 -0500

    lc6258 finished

--- leetcode/src/pass/weekly_contest/wc2022_12_10/lc6258.java
+++ leetcode/src/pass/weekly_contest/wc2022_12_10/lc6258.java
@@ -6,12 +6,6 @@ import java.util.*;
 public class lc6258 {
     public int longestSquareStreak(int[] nums) {
         Arrays.sort(nums);
-        int n = nums.length;
-        int ans = 0;
-        for (int i = 0; i < n; i++){
-            
-            int num = nums[i];
-            
-        }
+        return 0;
     }
 }
