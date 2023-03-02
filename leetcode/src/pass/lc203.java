df31afdf5e9301b01814bfaaa91f8694b312f243
ZBurNIngram <dumbledorebryant@gmail.com>
2023-03-01 22:56:10 -0500

    lc203 finished

---
+++ leetcode/src/pass/lc203.java
@@ -0,0 +1,22 @@
+package pass;
+
+public class lc203 {
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) return null;
+        if (head.val == val) return removeElements(head.next, val);
+        ListNode ans = head;
+        ListNode prev = head;
+        ListNode cur = head.next;
+        while (cur != null){
+            if (cur.val == val){
+                prev.next = cur.next;
+                cur = cur.next;
+            }
+            else {
+                cur = cur.next;
+                prev = prev.next;
+            }
+        }
+        return ans;
+    }
+}
