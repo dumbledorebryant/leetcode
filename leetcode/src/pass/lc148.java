0a274c32fb7d8a5fd5ac169f4d058a51a25266ea
ZBurNIngram <dumbledorebryant@gmail.com>
2023-03-29 16:53:28 -0400

    lc148 finished

--- leetcode/src/pass/lc148.java
+++ leetcode/src/pass/lc148.java
@@ -1,41 +1,23 @@
 package pass;
+
+import java.util.PriorityQueue;
+
 public class lc148 {
     public ListNode sortList(ListNode head) {
-        return sort(head, null);
-    }
-    public ListNode sort(ListNode head, ListNode tail){
-        if (head == null || head.next == null) return head;
-        ListNode slow = head;
-        ListNode fast = head;
-        while (fast != tail){
-            slow = slow.next;
-            fast = fast.next;
-            if (fast.next != tail){
-                fast = fast.next;
-            }
-            else break;
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        ListNode temp = head;
+        while (temp != null){
+            queue.add(temp.val);
+            temp = temp.next;
         }
 
-        ListNode left = sort(head, slow);
-        ListNode right = sort(slow.next, tail);
-        ListNode ans = new ListNode(0);
-        ListNode ptr = ans;
-        
-        while (left != null && right != null){
-            ListNode temp = new ListNode(0);
-            if (left.val < right.val){
-                temp.val = left.val;
-                left = left.next;
-            }
-            else {
-                temp.val = right.val;
-                right = right.next;
-            }
-            ptr.next = temp;
-            ptr = ptr.next;
+        ListNode dummy = new ListNode(Integer.MIN_VALUE);
+        ListNode next = dummy;
+        while (!queue.isEmpty()){
+            ListNode cur = new ListNode(queue.poll());
+            next.next = cur;
+            next = next.next;
         }
-        if (left == null) ptr.next = right;
-        if (right == null) ptr.next = left;
-        return ans.next;
+        return dummy.next;
     }
 }
