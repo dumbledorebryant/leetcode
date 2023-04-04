0de2988add091c921599e12225aaf5a18f199825
ZBurNIngram <dumbledorebryant@gmail.com>
2023-04-04 15:40:14 -0400

    lc560 finished

--- leetcode/src/pass/lc560.java
+++ leetcode/src/pass/lc560.java
@@ -3,20 +3,15 @@ package pass;
 public class lc560 {
     public int subarraySum(int[] nums, int k) {
         int length = nums.length;
-        int left = 0;
-        int right = 0;
-        int sum = 0;
         int count = 0;
-        while (right < length){
-            int num = nums[right];
-            sum += num;
-            while (sum >= k){
-                if (sum == k) count++;
-                int pre = nums[left];
-                sum -= pre;
-                left++;
+        int[] prefix = new int[length + 1];
+        for (int i = 1; i <= length; i++){
+            prefix[i] = prefix[i - 1] + nums[i - 1];
+        }
+        for (int i = 0; i < length; i++){
+            for (int j = i; j < length; j++){
+                if (prefix[j + 1] - prefix[i] == k) count++;
             }
-            right++;
         }
         return count;
     }
