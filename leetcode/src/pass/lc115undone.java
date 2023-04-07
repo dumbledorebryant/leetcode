0277932ceedcccf178a1ae8bdcf44006978f0d65
ZBurNIngram <dumbledorebryant@gmail.com>
2023-04-07 13:56:43 -0400

    lc115 finished

--- leetcode/src/pass/lc115undone.java
+++ leetcode/src/pass/lc115undone.java
@@ -2,6 +2,25 @@ package pass;
 
 public class lc115undone {
     public int numDistinct(String s, String t) {
-        return 0;
+        int m = s.length();
+        int n = t.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++){
+            dp[i][0] = 1;
+        }
+        for (int j = 1; j <= n; j++){
+            dp[0][j] = 0;
+        }
+        for (int i = 1; i <= m; i++){
+            char sch = s.charAt(i - 1);
+            for (int j = 1; j <= n; j++){
+                char tch = t.charAt(j - 1);
+                if (sch == tch){
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                }
+                else dp[i][j] = dp[i - 1][j];
+            }
+        }
+        return dp[m][n];
     }
 }
