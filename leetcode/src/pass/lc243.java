a00a4d9cf11a5f5b85da2c0e1bfccf5b10a9a489
ZBurNIngram <dumbledorebryant@gmail.com>
2023-04-27 17:19:31 -0400

    lc243 finished

---
+++ leetcode/src/pass/lc243.java
@@ -0,0 +1,24 @@
+package pass;
+
+public class lc243 {
+    public int shortestDistance(String[] wordsDict, String word1, String word2) {
+        int n = wordsDict.length;
+        int a = -n;
+        int b = 2 * n;
+        int ptr = 0;
+        int ans = n;
+        while (ptr < n){
+            if (wordsDict[ptr].equals(word1))
+                a = ptr;
+            if (wordsDict[ptr].equals(word2))
+                b = ptr;
+            int num = Math.abs(b - a);
+            if (num > 0){
+                ans = Math.min(ans, num);
+            }
+                
+            ptr++;
+        }
+        return ans;
+    }
+}
