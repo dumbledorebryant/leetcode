7ac3a372e7f138a0048a71f7689c07448aac75e9
ZBurNIngram <dumbledorebryant@gmail.com>
2023-05-01 15:56:03 -0400

    lc34 finished

--- leetcode/src/pass/lc34.java
+++ leetcode/src/pass/lc34.java
@@ -28,4 +28,46 @@ public class lc34 {
         }
         return new int[]{-1, -1};
     }
+
+    public int[] searchRanges(int[] nums, int target) {
+        int length = nums.length;
+        int left = 0;
+        int right = length - 1;
+        while (left <= right){
+            int mid = left + (right - left)/2;
+            int mnum = nums[mid];
+           
+            if (mnum == target){
+                right = mid - 1;
+            }
+            if (mnum > target){
+                right = mid - 1;
+            }
+            if (mnum < target){
+                left = mid + 1;
+            }
+        }
+        
+        int ansl = left;
+        if (left == length || nums[left] != target) return new int[]{-1, -1};
+        left = 0; 
+        right = length - 1;
+        while (left <= right){
+            int mid = left + (right - left)/2;
+            int mnum = nums[mid];
+           
+            if (mnum == target){
+                left = mid + 1;
+            }
+            if (mnum > target){
+                right = mid - 1;
+            }
+            if (mnum < target){
+                left = mid + 1;
+            }
+        }
+        int ansr = right;
+        if (right == -1 || nums[right] != target) return new int[]{-1, -1};
+        return new int[]{ansl, ansr};
+    }
 }
