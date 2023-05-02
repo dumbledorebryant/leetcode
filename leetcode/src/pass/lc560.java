20c189fc94c408f585a7e69975fef574c381d271
ZBurNIngram <dumbledorebryant@gmail.com>
2023-05-01 22:38:21 -0400

    lc560 finished

--- leetcode/src/pass/lc560.java
+++ leetcode/src/pass/lc560.java
@@ -1,5 +1,7 @@
 package pass;
 
+import java.util.*;
+
 public class lc560 {
     public int subarraySum(int[] nums, int k) {
         int length = nums.length;
@@ -12,6 +14,23 @@ public class lc560 {
             for (int j = i; j < length; j++){
                 if (prefix[j + 1] - prefix[i] == k) count++;
             }
+        }
+        return count;
+    }
+
+    public int subSum(int[] nums, int k){
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        int presum = 0;
+        int count = 0;
+        for (int num : nums){
+            presum += num;
+            if (map.containsKey(presum - k)){
+                count += map.get(presum - k);
+            }
+            
+            map.put(presum, map.getOrDefault(presum, 0) + 1);
         }
         return count;
     }
