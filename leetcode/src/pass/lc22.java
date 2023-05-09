a844f72b0229bbb4d82bfd620b0a8ddb1dc6d944
ZBurNIngram <dumbledorebryant@gmail.com>
2023-05-09 15:06:22 -0400

    lc22 finished

--- leetcode/src/pass/lc22.java
+++ leetcode/src/pass/lc22.java
@@ -26,4 +26,28 @@ public class lc22 {
         backtrace(left, right - 1, str);
         str = str.substring(0, str.length() - 1);
     }
+
+    List<String> anss = new ArrayList<>();
+    public List<String> generateP(int n) {
+        if (n == 0) return anss;
+        StringBuilder sb = new StringBuilder();
+        sb.append('(');
+        backtrace(n - 1, n, sb);
+        return ans;
+    }
+
+    private void backtrace(int left, int right, StringBuilder sb){
+        if (left < 0 || right < 0) return;
+        if (left > right) return;
+        if (left == 0 && right == 0) {
+            anss.add(sb.toString());
+            return;
+        }
+        sb.append('(');
+        backtrace(left - 1, right, sb);
+        sb.deleteCharAt(sb.length() - 1);
+        sb.append(')');
+        backtrace(left, right - 1, sb);
+        sb.deleteCharAt(sb.length() - 1);
+    }
 }
