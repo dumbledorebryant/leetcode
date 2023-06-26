dd81719e33e2180d2d40a2e98fd5b2dc7da0100c
ZBurNIngram <dumbledorebryant@gmail.com>
2023-06-26 16:56:50 -0400

    Blackrock OA finished

---
+++ leetcode/src/pass/Company/blackrock/BR1.py
@@ -0,0 +1,41 @@
+import sys
+stack = []
+flag = True
+valid_list = ['(', ')', '[', ']', '{', '}']
+
+for line in sys.stdin:
+    line = line.strip()
+    count = 0
+    for i in line:
+        if i not in valid_list:
+            continue
+        if i == '(':
+            stack.append(')')
+            count += 1
+        elif i == '[':
+            stack.append(']')
+        elif i == '{':
+            stack.append('}')
+        else:
+            if stack == []:
+                flag = False
+                print("InValid")
+                break
+            elif i != stack[-1]:
+                flag = False
+                print("InValid")
+                break
+            elif i == ')' and i == stack[-1]:
+                count -= 1
+                stack.pop()
+            else:
+                stack.pop()
+    if count != 0 and flag == True:
+        flag = False
+        print("InValid")
+        break
+if flag == True:
+    if stack == []:
+        print("Valid")
+    else:
+        print("InValid")
---
+++ leetcode/src/pass/Company/blackrock/BR_0.py
@@ -0,0 +1,53 @@
+import collections
+import sys
+import heapq
+temp_list = []
+query = []
+for line in sys.stdin:
+    if not temp_list:
+        temp_list.append(line.strip())
+    elif not query:
+        query.append(line.strip())
+    elif len(query) == 1:
+        query.append(line.strip())
+        break
+exchange_list = []
+for i in temp_list[0].split(";"):
+    exchange_list.append(i.split(","))
+
+graph = collections.defaultdict(dict)
+for i in exchange_list:
+    x, y, v = i[0], i[1], float(i[2])
+    graph[x][y] = 1 / v
+    graph[y][x] = v
+
+start = query[0]
+end = query[1]
+n = len(graph)
+heap = [(1.0, start, n + 1)]
+visited = collections.defaultdict(int)
+result = []
+while heap:
+    cost, x, n = heapq.heappop(heap)
+    if x == end:
+        result.append(1 / cost)
+    if visited[x] >= n:
+        continue
+    visited[x] = n
+    for y, d_cost in graph[x].items():
+        heapq.heappush(heap, (cost * d_cost, y, n - 1))
+print(result)
+if result == []:
+    print(-1.0)
+else:
+    print(max(result))
+
+
+
+
+
+
+
+
+
+
