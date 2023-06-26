1c138281e60b4d39265c71e8ac190e9301db49b0
ZBurNIngram <dumbledorebryant@gmail.com>
2023-06-26 18:33:26 -0400

    Blackrock OA finished

--- leetcode/src/pass/Company/blackrock/BR1.py
+++ leetcode/src/pass/Company/blackrock/BR1.py
@@ -19,11 +19,11 @@ for line in sys.stdin:
         else:
             if stack == []:
                 flag = False
-                print("InValid")
+                print("invalid")
                 break
             elif i != stack[-1]:
                 flag = False
-                print("InValid")
+                print("invalid")
                 break
             elif i == ')' and i == stack[-1]:
                 count -= 1
@@ -32,10 +32,10 @@ for line in sys.stdin:
                 stack.pop()
     if count != 0 and flag == True:
         flag = False
-        print("InValid")
+        print("invalid")
         break
 if flag == True:
     if stack == []:
-        print("Valid")
+        print("valid")
     else:
-        print("InValid")
+        print("invalid")
