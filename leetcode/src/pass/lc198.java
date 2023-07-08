6eadae119496390f6be1fd23749046902584b6b8
ZBurNIngram <dumbledorebryant@gmail.com>
2023-07-08 16:41:08 -0400

    lc198 finished

--- leetcode/src/pass/lc198.java
+++ leetcode/src/pass/lc198.java
@@ -13,4 +13,16 @@ public class lc198 {
         }
         return money[length];
     }
+
+    public int rob2(int[] nums){
+        int length = nums.length;
+        int[] dp = new int[length + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for (int i = 2; i <= length; i++){
+            int num = nums[i - 1];
+            dp[i] = Math.max(num + dp[i - 2], dp[i - 1]);
+        }
+        return dp[length];
+    }
 }
