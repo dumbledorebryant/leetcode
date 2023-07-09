fb4f1c9e3038c7c51c894c5b2302ea5cdf9b52a4
ZBurNIngram <dumbledorebryant@gmail.com>
2023-07-09 17:23:23 -0400

    lc365 finished

--- leetcode/src/pass/lc365.java
+++ leetcode/src/pass/lc365.java
@@ -1,5 +1,10 @@
 package pass;
 
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+
 public class lc365 {
     public boolean canMeasureWater(int jug1Capacity, int jug2Capacity, int targetCapacity) {
         if (jug1Capacity + jug2Capacity < targetCapacity) return false;
@@ -20,5 +25,41 @@ public class lc365 {
         }
         
         return false;
+    }
+
+    public boolean canMeasureWater2(int x, int y, int z) {
+        Deque<int[]> stack = new LinkedList<int[]>();
+        stack.push(new int[]{0, 0});
+        Set<Long> seen = new HashSet<Long>();
+        while (!stack.isEmpty()) {
+            if (seen.contains(hash(stack.peek()))) {
+                stack.pop();
+                continue;
+            }
+            seen.add(hash(stack.peek()));
+            
+            int[] state = stack.pop();
+            int remain_x = state[0], remain_y = state[1];
+            if (remain_x == z || remain_y == z || remain_x + remain_y == z) {
+                return true;
+            }
+            // 把 X 壶灌满。
+            stack.push(new int[]{x, remain_y});
+            // 把 Y 壶灌满。
+            stack.push(new int[]{remain_x, y});
+            // 把 X 壶倒空。
+            stack.push(new int[]{0, remain_y});
+            // 把 Y 壶倒空。
+            stack.push(new int[]{remain_x, 0});
+            // 把 X 壶的水灌进 Y 壶，直至灌满或倒空。
+            stack.push(new int[]{remain_x - Math.min(remain_x, y - remain_y), remain_y + Math.min(remain_x, y - remain_y)});
+            // 把 Y 壶的水灌进 X 壶，直至灌满或倒空。
+            stack.push(new int[]{remain_x + Math.min(remain_y, x - remain_x), remain_y - Math.min(remain_y, x - remain_x)});
+        }
+        return false;
+    }
+
+    public long hash(int[] state) {
+        return (long) state[0] * 1000001 + state[1];
     }
 }
