f3ea368440d49bc9661258a96858d109c5d784b0
ZBurNIngram <dumbledorebryant@gmail.com>
2023-08-28 13:48:33 -0400

    add lc354 new method

--- leetcode/src/pass/lc354.java
+++ leetcode/src/pass/lc354.java
@@ -44,4 +44,41 @@ public class lc354 {
         }
         return piles;
     }
+
+    public int max(int[][] envelopes) {
+        Arrays.sort(envelopes, (a, b) -> {
+            if (a[0] == b[0]) return b[1] - a[1];
+            return a[0] - b[0];
+        });
+        int n = envelopes.length;
+        int[] piles = new int[n];
+        int[] list = new int[n];
+        for (int i = 0; i < n; i++){
+            list[i] = envelopes[i][1];
+        }
+
+        return countPiles(piles, list);
+    }
+
+    public int countPiles(int[] piles, int[] list){
+        int n = piles.length;
+        int m = 0;
+        for (int i = 0; i < n; i++){
+            int left = 0;
+            int right = m - 1;
+            int num = list[i];
+            while (left <= right){
+                int mid = left + (right - left) / 2;
+                if (piles[mid] < num) {
+                    left = mid + 1;
+                }
+                else {
+                    right = mid - 1;
+                }
+            }
+            if (left == m) m++;
+            piles[left] = num; 
+        }
+        return m;
+    }
 }
