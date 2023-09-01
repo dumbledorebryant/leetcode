c1262151b7f0674ddbd9d27efce892ddd1578fb1
ZBurNIngram <dumbledorebryant@gmail.com>
2023-09-01 17:01:45 -0400

    add tt oa No.4

---
+++ leetcode/src/pass/Company/Tiktok/ng24/maxTaskPriority.java
@@ -0,0 +1,36 @@
+package pass.Company.Tiktok.ng24;
+import java.util.*;
+public class maxTaskPriority {
+    public maximumPriority(int n, int[] priority, int x, int y){
+        Arrays.sort(priority);
+        int length = priority.length;
+        int left = 0; int right = length - 1;
+        while (left <= right){
+            int temp = priority[left];
+            priority[left] = priority[right];
+            priority[right] = temp;
+            left++;
+            right--;
+        }
+
+        int sum = 0;
+        int cd = 0;
+
+        for (int i = 0; i < y; i++){
+            if (cd == 0) {
+                sum += priority[cd];
+            }
+            else if (cd == x + 1){
+                cd = 0;
+                sum += priority[cd];
+            }
+            else {
+                if (cd <= n - 1){
+                    sum += priority[cd];
+                }
+            }
+            cd++;
+        }
+        return sum;
+    }
+}
