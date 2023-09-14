a2223de07885ce36b7cfbb5fa169552d91b15c74
ZBurNIngram <dumbledorebryant@gmail.com>
2023-09-14 16:56:15 -0400

    lc1008 finished

---
+++ leetcode/src/pass/lc1008.java
@@ -0,0 +1,25 @@
+package pass;
+
+public class lc1008 {
+    public TreeNode bstFromPreorder(int[] preorder) {
+        int n = preorder.length;
+        return bst(preorder, 0, n - 1);
+    }
+
+    public TreeNode bst(int[] nodes, int i, int j){
+        if (i > j) return null;
+        int rnum = nodes[i];
+        TreeNode root = new TreeNode(rnum);
+        boolean flag = true;
+        int k = i + 1;
+        for (; k <= j; k++){
+            int num = nodes[k];
+            if (num > rnum){
+                break;
+            }
+        }
+        root.left = bst(nodes, i + 1, k - 1);
+        root.right = bst(nodes, k, j);
+        return root;
+    }
+}
