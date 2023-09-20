c1f9264c173eb183a3991ed827f0ace791120046
ZBurNIngram <dumbledorebryant@gmail.com>
2023-09-19 23:25:10 -0400

    lc637 finished

--- leetcode/src/pass/lc637.java
+++ leetcode/src/pass/lc637.java
@@ -1,7 +1,24 @@
 package pass;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class lc637 {
-    public int findNumberOfLIS(int[] nums) {
-        return 0;
+    public List<Double> averageOfLevels(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>(List.of(root));
+        List<Double> ans = new ArrayList<>();
+        while (q.size() > 0) {
+            double qlen = q.size(), row = 0;
+            for (int i = 0; i < qlen; i++) {
+                TreeNode curr = q.poll();
+                row += curr.val;
+                if (curr.left != null) q.offer(curr.left);
+                if (curr.right != null) q.offer(curr.right);
+            }
+            ans.add(row/qlen);
+        }
+        return ans;
     }
 }
