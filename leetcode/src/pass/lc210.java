4bc0c3f74244adcb88f6045de9ccaf433424cd17
ZBurNIngram <dumbledorebryant@gmail.com>
2023-10-10 10:36:20 -0400

    lc210 finished

--- leetcode/src/pass/lc210.java
+++ leetcode/src/pass/lc210.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class lc210 {
     boolean flag = false;
@@ -54,4 +55,50 @@ public class lc210 {
         postorder.add(node);
         onpath[node] = false;
     }
+
+        // 主函数
+    public int[] findOrder2(int numCourses, int[][] prerequisites) {
+        // 建图，和环检测算法相同
+        List<Integer>[] graph = buildGraph(numCourses, prerequisites);
+        // 计算入度，和环检测算法相同
+        int[] indegree = new int[numCourses];
+        for (int[] edge : prerequisites) {
+            int from = edge[1], to = edge[0];
+            indegree[to]++;
+        }
+
+        // 根据入度初始化队列中的节点，和环检测算法相同
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.offer(i);
+            }
+        }
+
+        // 记录拓扑排序结果
+        int[] res = new int[numCourses];
+        // 记录遍历节点的顺序（索引）
+        int count = 0;
+        // 开始执行 BFS 算法
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            // 弹出节点的顺序即为拓扑排序结果
+            res[count] = cur;
+            count++;
+            for (int next : graph[cur]) {
+                indegree[next]--;
+                if (indegree[next] == 0) {
+                    q.offer(next);
+                }
+            }
+        }
+
+        if (count != numCourses) {
+            // 存在环，拓扑排序不存在
+            return new int[]{};
+        }
+        
+        return res;
+    }
+
 }
