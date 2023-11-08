73f36e4b8cd0cd092f231c3393942eab0729d1be
ZBurNIngram <dumbledorebryant@gmail.com>
2023-11-08 16:08:08 -0500

    lc1265 finished

---
+++ leetcode/src/pass/lc1265.java
@@ -0,0 +1,9 @@
+package pass;
+
+public class lc1265 {
+    public void printLinkedListInReverse(ImmutableListNode head) {
+        if (head == null) return;
+        printLinkedListInReverse(head.getNext());
+        head.printValue();
+    }
+}
