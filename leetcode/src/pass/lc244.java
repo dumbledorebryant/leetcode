d8c4675e72bc6da6fb1c739aa2b06a398feffe98
ZBurNIngram <dumbledorebryant@gmail.com>
2023-11-17 15:45:44 -0500

    lc244 finished

---
+++ leetcode/src/pass/lc244.java
@@ -0,0 +1,41 @@
+package pass;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class lc244 {
+    Map<String, List<Integer>> map = new HashMap<>();
+    public WordDistance(String[] wordsDict) {
+        int n = wordsDict.length;
+        for (int i = 0; i < n; i++){
+            String str = wordsDict[i];
+            map.putIfAbsent(str, new ArrayList<>());
+            map.get(str).add(i);
+        }
+    }
+    
+    public int shortest(String word1, String word2) {
+        List<Integer> a = map.get(word1);
+        List<Integer> b = map.get(word2);
+        int m = a.size();
+        int n = b.size();
+
+        int i = 0;
+        int j = 0;
+        int ans = Integer.MAX_VALUE;
+        while (i < m && j < n){
+            int x = a.get(i);
+            int y = b.get(j);
+            if (x < y){
+                i++;
+            }
+            else {
+                j++;
+            }
+            ans = Math.min(ans, Math.abs(x - y));
+        }
+        return ans;
+    }
+}
