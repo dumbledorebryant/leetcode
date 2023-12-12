1bdac67dc520700c2064bcfdf7d6f9f713cf5662
ZBurNIngram <dumbledorebryant@gmail.com>
2023-12-12 15:50:33 -0500

    lc718 finished

---
+++ leetcode/src/pass/lc718.java
@@ -0,0 +1,23 @@
+public class lc718 {
+    public int findLength(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        int max = 0;
+
+        for (int i = 1; i <= m; i++){
+            for (int j = 1; j <= n; j++){
+                if (nums1[i - 1] == nums2[j - 1]){
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else {
+                    dp[i][j] = 0;
+                }
+                max = Math.max(max, dp[i][j]);
+            }
+        }
+        return max;
+    }
+}
