1689ea68871fdaa9f5bed1b0d1741b0bb98c6890
ZBurNIngram <dumbledorebryant@gmail.com>
2024-01-15 20:59:24 -0500

    lc258 finished

---
+++ leetcode/src/pass/lc258.java
@@ -0,0 +1,15 @@
+package pass;
+
+public class lc258 {
+    public int addDigits(int num) {
+        while (num >= 10){
+            int next = 0;
+            while (num > 0){
+                next += (num % 10);
+                num = num / 10;
+            }
+            num = next;
+        }
+        return num;
+    }
+}
