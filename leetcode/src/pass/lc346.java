53e08a4e718d5c3e73f05f82395c33c261dd0c5b
ZBurNIngram <dumbledorebryant@gmail.com>
2024-01-16 10:05:54 -0500

    lc346 finished

---
+++ leetcode/src/pass/lc346.java
@@ -0,0 +1,25 @@
+package pass;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class lc346 {
+    Deque<Integer> dq = new ArrayDeque<>();
+    int sum = 0;
+    int sz;
+    public MovingAverage(int size) {
+        sz = size;
+    }
+    
+    public double next(int val) {
+        sum += val;
+        dq.addLast(val);
+        if (dq.size() > sz){
+            sum -= dq.removeFirst();
+            return sum * 1.0 / sz;
+        }
+        else {
+            return sum * 1.0 / dq.size();
+        }
+    }
+}
