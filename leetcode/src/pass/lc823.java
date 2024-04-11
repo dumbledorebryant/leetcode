eef8da9884e20e269886e6c9f3a5b4c7e6fd3218
ZBurNIngram <dumbledorebryant@gmail.com>
2024-04-11 12:42:51 -0400

    add lc823

---
+++ leetcode/src/pass/lc823.java
@@ -0,0 +1,40 @@
+package pass;
+
+public class lc823 {
+    final static int mod = (int)(1e9 + 7);
+    Map<Integer, Integer> map = new HashMap<>();
+    long[] list;
+    public int numFactoredBinaryTrees(int[] arr) {
+        int n = arr.length;
+        Arrays.sort(arr);
+        for (int i = 0; i < n; i++){
+            map.put(arr[i], i);
+        }
+        list = new long[n];
+       
+        long ans = 0;
+        for (int i = 0; i < n; i++){
+            ans += dp(arr, i);
+        }
+        return (int)(ans % mod);
+    }
+
+    public long dp(int[] arr, int idx){
+        
+        if (list[idx] != 0) return list[idx];
+        int num = arr[idx];
+        long res = 1;
+        
+        for (int i = 0; i < idx; i++){
+            int cur = arr[i];
+            int left = num % cur;
+            int pair = num / cur;
+            if (left == 0 && map.containsKey(pair)){
+                res += ((dp(arr, i) * dp(arr, map.get(pair))) % mod);
+            }
+        }
+        res %= mod;
+        list[idx] = res;
+        return res;
+    }
+}
